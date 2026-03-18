5341ff4e1f4e05fda2623da2d3f5de97f4c8d523
Bilawal Malik <bilawal@unknownb6792caa028e.attlocal.net>
2026-03-17 21:37:21 -0500

    new update

--- home.py
+++ home.py
@@ -16,40 +16,40 @@ account_opened = False
 
 def open_account():
     global balance, account_opened, name, username
-    name = input("What's your name: ").capitalize()
-    print(f'Heyyy! Welcome {name}.\n')
+    name = input("\nWhat's your name: ").capitalize()
+    print(f'\nHeyyy! Welcome {name}.\n')
     print(f'You must deposit $20 or more to create an account.\n')
     while True:
         consent = input("Do you want to create an account? [yes/no]: ").lower()
         if consent in ["yes", "no"]:
             break
-        print("Please type yes or no.")
+        print("\nPlease type yes or no.\n")
     if consent == "yes":
         while True:
             try:
-                username = input("Create a username : ")
+                username = input("\nCreate a username : ")
                 cur = conn.cursor()
                 sql_1 = "SELECT username from accounts WHERE username=%s"
-                data_1 = (username)
+                data_1 = (username,)
                 cur.execute(sql_1, data_1)
                 existing_user = cur.fetchone()
                 if existing_user:
-                    print("Sorry! , this username is taken try another one.")
+                    print("\nSorry! , this username is taken try another one.")
                     cur.close()
                     continue
                 else:
-                    print("Username created")
-                print(f"Now create a 4-digit Pin [eg: 1234].")
-                pin = input("Enter a pin : ")
+                    print("\nUsername created")
+                print(f"\nNow create a 4-digit Pin [eg: 1234].")
+                pin = input("\nEnter a pin : ")
                 while not (pin.isdigit()) and not (len(pin) == 4):
-                    print("A four digit pin please [eg 1234]")
-                    pin = input("Enter a pin : ")
-                print("Pin created.")
+                    print("\nA four digit pin please [eg 1234]")
+                    pin = input("\nEnter a pin : ")
+                print("\nPin created.")
                 initial_deposit = float(
-                    input("How much would you like to deposit: "))
+                    input("\nHow much would you like to deposit: "))
 
                 if initial_deposit < 20:
-                    print("Error: Minimum deposit is $20. Please try again.")
+                    print("\nError: Minimum deposit is $20. Please try again.")
                     continue
                 balance += initial_deposit
                 account_opened = True
@@ -61,22 +61,22 @@ def open_account():
                     conn.commit()
                     cur.close()
                 except Exception as e:
-                    print(f"Failed to save account: {e}")
+                    print(f"\nFailed to save account: {e}")
                     conn.rollback()
 
-                print(f'Congratulations {name}! Your account is created.')
-                print(f'Current balance: ${balance:,.2f}')
+                print(f'\nCongratulations {name}! Your account is created.')
+                print(f'\nCurrent balance: ${balance:,.2f}')
                 break
             except ValueError:
-                print("Invalid input. Please enter a number (e.g., 20 or 25.75).")
+                print("\nInvalid input. Please enter a number (e.g., 20 or 25.75).")
     else:
         exit()
 
 
 def signin():
     global balance, account_opened, name, username
-    login_name = input("Enter your username: ")
-    login_pin = input("Enter your pin: ")
+    login_name = input("\nEnter your username: ")
+    login_pin = input("\nEnter your pin: ")
 
     try:
         cur = conn.cursor()
@@ -89,25 +89,25 @@ def signin():
             balance = float(result[1])
             username = login_name
             account_opened = True
-            print(f"Login successful! Welcome back, {name}.")
-            print(f"Account Balance: ${balance:,.2f}")
+            print(f"\nLogin successful! Welcome back, {name}.")
+            print(f"\nAccount Balance: ${balance:,.2f}")
             cur.close()
         else:
-            print("Invalid username or pin. Please try again.")
+            print("\nInvalid username or pin. Please try again.")
             cur.close()
     except Exception as e:
-        print(f"Database error: {e}")
+        print(f"\nDatabase error: {e}")
 
 
 def deposit():
     global balance
     while True:
         try:
-            user_input = input("Enter the amount you want to deposit: ")
+            user_input = input("\nEnter the amount you want to deposit: ")
             amount = float(user_input)
 
             if amount <= 0:
-                print("Please enter an amount greater than 0.")
+                print("\nPlease enter an amount greater than 0.")
                 continue
 
             balance += amount
@@ -116,19 +116,19 @@ def deposit():
                 sql_4 = "UPDATE accounts SET balance = %s WHERE username = %s"
                 data_4 = (balance, username)
                 cur.execute(sql_4, data_4)
-                cur.commit()
+                conn.commit()
                 cur.close()
                 break
             except Exception as e:
-                print(f"Failed to save account: {e}")
+                print(f"\nFailed to save account: {e}")
                 conn.rollback()
 
             print(
-                f'Deposit successful. Current balance: ${balance:,.2f}')
+                f'\nDeposit successful. Current balance: ${balance:,.2f}')
             break
 
         except ValueError:
-            print("Invalid input. Please use numbers (e.g., 50 or 90.75).")
+            print("\nInvalid input. Please use numbers (e.g., 50 or 90.75).")
 
 
 def withdraw():
@@ -136,21 +136,21 @@ def withdraw():
     while True:
         try:
             withdrawal = float(
-                input("Enter the amount you want to withdraw: "))
+                input("\nEnter the amount you want to withdraw: "))
             if withdrawal > balance:
-                print("Request Declined: Insufficent balance")
-                print(f"Your account balance is {balance:,.2f}.")
+                print("\nRequest Declined: Insufficent balance")
+                print(f"\nYour account balance is {balance:,.2f}.")
                 ask = input(
-                    "You want to withdraw a different amount? [yes/no]")
+                    "\nYou want to withdraw a different amount? [yes/no]")
                 while ask not in ["yes", "no"]:
                     ask = input(
-                        "You want to withdraw a different amount? [yes/no]")
+                        "\nYou want to withdraw a different amount? [yes/no]")
                 if ask == "yes":
                     continue
                 else:
                     return
             if withdrawal <= 0:
-                print("Please enter a positive amount.")
+                print("\nPlease enter a positive amount.")
                 continue
             balance -= withdrawal
             try:
@@ -158,51 +158,54 @@ def withdraw():
                 sql_5 = "UPDATE accounts SET balance = %s WHERE username = %s"
                 data_5 = (balance, username)
                 cur.execute(sql_5, data_5)
-                cur.commit()
+                conn.commit()
                 cur.close()
+                print(
+                    f"\nWithdrawal successful. Your remaining balance = ${balance:,.2f}")
                 break
             except Exception as e:
-                print(f"Failed to save account: {e}")
+                print(f"\nFailed to save account: {e}")
                 conn.rollback()
-
-            print(
-                f'Withdrawal successful. Your remaining balance: ${balance:,.2f}')
-            break
         except ValueError:
-            print("Invalid input. Please use numbers (e.g., 50 or 90.75).")
+            print("\nInvalid input. Please use numbers (e.g., 50 or 90.75).")
 
 
 def print_balance():
     global balance
-    print(f'Current balance = ${balance:,.2f}')
+    print(f'\nCurrent balance = ${balance:,.2f}')
 
 
 def exit_program():
-    print("bye have a good day.")
+    print("\nbye have a good day.")
     sys.exit()
 
 
 def ask():
-    print(f"{'-'*40}")
-    print(f'What would you like to do {name}.')
-    print("OPTIONS")
-    print("1) Menu for [Deposit , Withdraw , Check balance]")
-    print("2) Exit")
-    asking = input("Choose [1 or 2] :")
-    if asking not in ["1", "2"]:
-        asking = input("Please Choose [1 or 2] :")
-    else:
-        exit_program()
-    if asking == "1":
+    while True:
         print(f"{'-'*40}")
-        print("OPTIONS")
+        print(f'What would you like to do {name}.')
+        print("OPTIONS\n")
+        print("1) Menu for [Deposit, Withdraw, Check balance]")
+        print("2) Exit")
+
+        asking = input("\nChoose [1 or 2] : ")
+        while asking not in ["1", "2"]:
+            asking = input("Please Choose [1 or 2] :")
+
+        if asking == "2":
+            exit_program()
+
+        print(f"{'-'*40}")
+        print("OPTIONS\n")
         print("1) Deposit")
         print("2) Withdraw")
         print("3) Check balance")
         print("4) Exit")
-        menu_choice = input(f'{name}. Choose [1 , 2 , 3 or 4]: ')
-        if menu_choice not in ["1", "2", "3", "4"]:
-            menu_choice = input(f'{name}.Please Choose [1 , 2 , 3 or 4]: ')
+
+        menu_choice = input(f'{name}. Choose [1, 2, 3 or 4]: ')
+        while menu_choice not in ["1", "2", "3", "4"]:
+            menu_choice = input(f'{name}. Please Choose [1, 2, 3 or 4]: ')
+
         if menu_choice == "1":
             deposit()
         elif menu_choice == "2":
@@ -213,28 +216,24 @@ def ask():
             exit_program()
 
 
-def main():
-    print(f"{'-'*40}")
-    print(f"{'-'*40}")
-    print(f"{'-'*11}CLI BANKING SYSTEM{'-'*11}")
-    print(f"{'-'*40}")
-    print(f"{'-'*40}")
-    print("OPTIONS")
-    print("1) Create a new account")
-    print("2) Sign into existing account")
-    print("3) Exit")
-    selection = input("Choose [1 , 2 or 3] :")
-    while selection not in ["1", "2", "3"]:
-        selection = input("Please Choose [1 , 2 or 3] :")
-    if selection == "1":
-        open_account()
-        ask()
-    elif selection == "2":
-        signin()
-        ask()
-    else:
-        exit()
-
-
-if __name__ == "__main__":
-    main()
+print(f"{'-'*40}")
+print(f"{'-'*40}")
+print(f"{'-'*11}CLI BANKING SYSTEM{'-'*11}")
+print(f"{'-'*40}")
+print(f"{'-'*40}")
+print("OPTIONS\n")
+print("1) Create a new account")
+print("2) Sign into existing account")
+print("3) Exit")
+print(f"{'-'*40}")
+selection = input("Choose [1 , 2 or 3] : ")
+while selection not in ["1", "2", "3"]:
+    selection = input("\nPlease Choose [1 , 2 or 3] : ")
+if selection == "1":
+    open_account()
+    ask()
+elif selection == "2":
+    signin()
+    ask()
+else:
+    exit_program()
